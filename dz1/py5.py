48b57207a0f3a811f52339962b3f0882fdf62477
Viktoria27010 <shilina.vv@phystech.edu>
2023-09-24 13:43:15 +0300

    ок

--- dz1/py5.py
+++ dz1/py5.py
@@ -1,13 +1,23 @@
-import matplotlib.pyplot as plt
 import numpy as np
-x = [1, 2, 3, 4, 5]
+import matplotlib.pyplot as plt
+x = [1.0, 2.0, 3.0, 4.0, 5.0]
 y = [0.99, 0.49, 0.35, 0.253, 0.18]
-plt.errorbar(x, y, xerr=0.05, yerr=0.1)
+p1 = []
+p2 = []
 p, v = np.polyfit(x, y, deg=1, cov=True)
-p_f = np.poly1d(p)
-z = p_f([1, 2, 3, 4, 5])
-plt.xlabel(r'$x$')
-plt.ylabel(r'$f(x)$')
-plt.title(r'График')
-plt.plot(x, eval('y'))
+p_f = str(np.poly1d(p)).split()
+for i in x:
+    p1.append(float(p_f[0])*i+float(p_f[3]))
+plt.plot(x, p1, color='green', label=r'$Аппроксимация\ по\ многочленам\ 1й\ степени$')
+
+p, v = np.polyfit(x, y, deg=2, cov=True)
+p_f = str(np.poly1d(p)).split()
+print(p_f)
+for i in x:
+    p2.append(float(p_f[1])*i - float(p_f[4])*i + float(p_f[-1]))
+plt.plot(x, p2, color='yellow', label=r'$Аппроксимация\ по\ многочленам\ 2й\ степени$')
+
+plt.errorbar(x, y, xerr=0.05, yerr=0.1, color='red')
+plt.legend(loc='best', fontsize=9)
+plt.grid()
 plt.show()
