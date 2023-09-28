36314f2428b61e023f5fe0e59f6f69bf9aae44e7
Viktoria27010 <shilina.vv@phystech.edu>
2023-09-28 19:12:02 +0300

    ок

--- dz2/task 4.py
+++ dz2/task 4.py
@@ -1,14 +1,11 @@
 import turtle
-
 for i in range(72):
     turtle.forward(5)
     turtle.left(5)
     turtle.forward(5)
 
-
+#2 variant
 import turtle
 t = turtle.Turtle()
-  
-  
 r = 50
 t.circle(r)
